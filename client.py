da207b5ca0197cc2cbf3e809086735d14e0e1912
eerdddfff <1924003122@qq.com>
2025-06-14 23:23:01 +0800

    Update client.py

--- client.py
+++ client.py
@@ -3,3 +3,15 @@ import sys
 import base64
 import time
 
+def send_and_receive(sock, msg, server, retries=5):
+    timeout = 1
+    for i in range(retries):
+        sock.sendto(msg.encode(), server)
+        sock.settimeout(timeout)
+        try:
+            data, addr = sock.recvfrom(2048)
+            return data.decode(), addr
+        except socket.timeout:
+            timeout *= 2
+    return None, None
+
