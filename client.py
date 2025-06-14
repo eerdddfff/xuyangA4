bf55bf4fad094c53da7f3c517545254fa9916a6f
eerdddfff <1924003122@qq.com>
2025-06-14 23:23:36 +0800

    Update client.py

--- client.py
+++ client.py
@@ -15,3 +15,23 @@ def send_and_receive(sock, msg, server, retries=5):
             timeout *= 2
     return None, None
 
+def download_file(server_host, port, file_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = (server_host, port)
+
+    msg = f"DOWNLOAD {file_name}"
+    response, _ = send_and_receive(sock, msg, server)
+    if not response:
+        print(f"Download request for {file_name} failed.")
+        return
+
+    if response.startswith("ERR"):
+        print(f"Server does not have file {file_name}")
+        return
+
+    parts = response.split()
+    size = int(parts[4])
+    data_port = int(parts[6])
+    data_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  
